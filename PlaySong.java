6f30fcd5b3a87ad044550c3daa58e61d0c976942
Sanket <saku18ec@cmrit.ac.in>
2021-08-31 21:07:37 +0530

    Intial commit

---
+++ PlaySong.java
@@ -0,0 +1,57 @@
+package com.example.isangeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class PlaySong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_song);
+
+        textView = findViewById(R.id.textView);
+        play = findViewById(R.id.play);
+        previous = findViewById(R.id.previous);
+        next = findViewById(R.id.next);
+
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        songs = (ArrayList) bundle.getParcelableArrayList("songList");
+        textContent = intent.getStringExtra("currentSong");
+        textView.setText(textContent);
+        textView.setSelected(true);
+        position = intent.getIntExtra("position", 0);
+        Uri uri = Uri.parse(songs.get(position).toString());
+        mediaPlayer = MediaPlayer.create(this, uri);
+        mediaPlayer.start();
+
+
+
+
+
+
+    }
+
+
+    TextView textView;
+    ImageView play, previous, next;
+    ArrayList<File> songs;
+    MediaPlayer mediaPlayer;
+    String textContent;
+    int position;
+    SeekBar seekBar;
+    Thread updateSeek;
+
+}
